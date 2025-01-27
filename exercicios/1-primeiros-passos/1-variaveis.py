05b1cfcbbb4975fc8fa21ec1acb32e8bb9229f25
vitschwenck <schwenckvitoria@gmail.com>
2025-01-27 18:50:37 +0000

    resolução variaveis

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -4,8 +4,10 @@
 # - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
 
 # Declare variáveis do tipo string
-
+'vitoria''paloma'
 # Declare variáveis de cada tipo numérico
-
+numero_inteiro = 12
+salario = 12352.00
 
 # Declare uma variável do tipo booleano
+vitoria linda = True
